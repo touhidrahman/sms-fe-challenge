f0962191bcd01fb7181b3d7ae09cf7867d57fbc4
Touhid Rahman <touhidrahman1987@gmail.com>
2019-03-25 10:32:15 +0100

    :tada: Setup angular and ng-material

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+    imports: [ CommonModule, MatButtonModule, MatCheckboxModule ],
+    exports: [ MatButtonModule, MatCheckboxModule ],
+})
+export class AngularMaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [ AppComponent ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        AngularMaterialModule,
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
